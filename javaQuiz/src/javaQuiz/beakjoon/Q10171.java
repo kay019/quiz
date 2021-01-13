7d30672c640b7b3c1a1a991043591d0af0f10682
kay019 <crowruya@gmail.com>
2021-01-13 22:45:56 +0900

    beakjoon q10171
    21.01.13

---
+++ javaQuiz/src/javaQuiz/beakjoon/Q10171.java
@@ -0,0 +1,17 @@
+package javaQuiz.beakjoon;
+
+public class Q10171 {
+
+	public static void main(String[] args) {
+
+		
+		//Q 고양이를 출력하시오
+		
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		
+	}
+
+}
