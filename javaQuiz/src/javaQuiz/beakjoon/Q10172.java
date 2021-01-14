8e608a0cbfe4253ff93e9b1105d6befc74ccd660
kay019 <crowruya@gmail.com>
2021-01-14 23:34:33 +0900

    beakjoon
    100, 1001, 10172

---
+++ javaQuiz/src/javaQuiz/beakjoon/Q10172.java
@@ -0,0 +1,18 @@
+package javaQuiz.beakjoon;
+
+public class Q10172 {
+
+	public static void main(String[] args) {
+
+		
+		//개를 출력하시오
+
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+		
+	}
+
+}
