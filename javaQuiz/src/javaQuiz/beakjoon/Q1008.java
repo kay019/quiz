b1567c8727f318fb8e94218a13d4e21f2f7ada43
kay019 <crowruya@gmail.com>
2021-01-16 23:54:33 +0900

    beakjoon
    1008
    10998

---
+++ javaQuiz/src/javaQuiz/beakjoon/Q1008.java
@@ -0,0 +1,27 @@
+package javaQuiz.beakjoon;
+
+import java.util.Scanner;
+
+public class Q1008 {
+
+	public static void main(String[] args) {
+
+		
+		//
+		
+		
+		Scanner sc = new Scanner(System.in);
+			
+			double a, b;
+			
+			 a = sc.nextDouble();
+			 b = sc.nextDouble();
+			 
+			 if(0 < a) {
+				 if(b < 10) {
+			 System.out.println(a/b);
+				 }
+			 }
+	}
+
+}
